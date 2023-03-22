1c74b8b3c747f55b8c4cfff7f3c3494a3309b5dd
FHP187 <gjimenez@jaumebalmes.net>
2023-03-22 20:58:26 +0100

    2n commit

--- 1/main.js
+++ 1/main.js
@@ -1,22 +1,18 @@
 export { all } from "pong.js";
 import "pong.js";
-import canvas from "pong.js";
-import context from "pong.js";
-import graella from "pong.js";
-import alçadaPala from "pong.js";
-import maximPalaY from "pong.js";
-import palaEsquerra from "pong.js";
-import velocitatPilota from "pong.js";
-import palaDreta from "pong.js";
-import paddleSpeed from "pong.js";
-import pilota from "pong.js";
+// import canvas from "pong.js";
+// import context from "pong.js";
+// import graella from "pong.js";
+// import alçadaPala from "pong.js";
+// import maximPalaY from "pong.js";
+// import palaEsquerra from "pong.js";
+// import velocitatPilota from "pong.js";
+// import palaDreta from "pong.js";
+// import paddleSpeed from "pong.js";
+// import pilota from "pong.js";
 
-import function collides(){
-
-}
-export function loop(){
-
-}
+collides();
+loop();
 requestAnimationFrame(loop);
 
 
