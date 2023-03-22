2fff71fc93f96bd5d7a692b8d9853e13dabb91d2
FHP187 <gjimenez@jaumebalmes.net>
2023-03-22 22:00:57 +0100

    2n commit

--- 1/main.js
+++ 1/main.js
@@ -11,8 +11,8 @@ import "pong.js";
 // import paddleSpeed from "pong.js";
 // import pilota from "pong.js";
 
-call.collides(obj1, obj2);
-call.loop();
+collides(obj1, obj2);
+loop();
 requestAnimationFrame(loop);
 
 
