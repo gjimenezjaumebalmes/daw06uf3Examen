0e376f868a97099d8e9be1b8945cd3c272d2e6ea
FHP187 <gjimenez@jaumebalmes.net>
2023-03-22 20:46:55 +0100

    2n commit

--- 3/mapreduce.js
+++ 3/mapreduce.js
@@ -1,6 +1,6 @@
 function potenciawithoutfilterandmap() {
 
-    const input = [1, 2, 3, 4, 5];
+    const input = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
     const pot = input.map(function (num) {
         return Math.pow(num, 2);
@@ -11,9 +11,9 @@ function potenciawithoutfilterandmap() {
         (accumulator, currentValue) => accumulator + currentValue,
         initialValue
     );
-
-    console.log(pots);
-    console.log(sumWithInitial);
+    console.log(pot); // Muestra valores potencia de 2
+    console.log(pots);  // Muestra valores potencia de 2 mas pequeños de 10
+    console.log(sumWithInitial); // Muestra valores potencia de 2 mas pequeños de 10 sumados
 
 }
     potenciawithoutfilterandmap();
